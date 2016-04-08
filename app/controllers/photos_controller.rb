97718816f214a19ce5c891da9a57622d4d70d78f
Seif <seif.dorani@gmail.com>
2016-04-08 12:47:30 -0400

    created association with users in routes, a user has many photos, then def photo_params for secure uploads, put it in the create action

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,16 @@
 class PhotosController < ApplicationController
+
 def new
  @photo = Photo.new
 end
 
+def create
+ current_user.photos.new(photo_params)
+end
+
+private
+
+def photo_params
+  params.require(:photo).permit(:file, :caption)
+ end
 end

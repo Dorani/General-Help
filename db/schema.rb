a96e31c79d70947a5e1d3b95ecf330a552fa09e6
Seif <seif.dorani@gmail.com>
2016-04-08 12:17:31 -0400

    migrated,created models, now need to update model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408143248) do
+ActiveRecord::Schema.define(version: 20160408161126) do
+
+  create_table "photos", force: :cascade do |t|
+    t.string   "file"
+    t.text     "caption"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "photos", ["user_id"], name: "index_photos_on_user_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "user_name"
